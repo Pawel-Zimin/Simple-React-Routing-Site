da26aaf38cb09c971d94773305f8d7919e19bff3
Pawel Zimin <paweu14@gmail.com>
2020-09-09 17:44:38 +0200

    Updates

---
+++ precache-manifest.a5fc1cffe5d0c260a53811660e5416fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cab6b043ed9655166ee7d679dfa4f91",
+    "url": "/Simple-React-Routing-Site/index.html"
+  },
+  {
+    "revision": "d7b393f5ededf1126136",
+    "url": "/Simple-React-Routing-Site/static/css/main.0e2f4e67.chunk.css"
+  },
+  {
+    "revision": "937bb83e4e7db685650b",
+    "url": "/Simple-React-Routing-Site/static/js/2.669c949d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Simple-React-Routing-Site/static/js/2.669c949d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b393f5ededf1126136",
+    "url": "/Simple-React-Routing-Site/static/js/main.0fe39760.chunk.js"
+  },
+  {
+    "revision": "bd7d3e5556071a58e8f4",
+    "url": "/Simple-React-Routing-Site/static/js/runtime-main.8055459f.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/Simple-React-Routing-Site/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/Simple-React-Routing-Site/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/Simple-React-Routing-Site/static/media/header3.d9d36b65.jpg"
+  }
+]);
